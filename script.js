fe43d7ba402eb3909cd376da8e7e0c590088940f
Riley Hansen <gridironhero@gmail.com>
2024-06-25 00:34:19 -0600

    customized home page

---
+++ script.js
@@ -0,0 +1,27 @@
+// Add your JavaScript here
+document.addEventListener("DOMContentLoaded", () => {
+  const mobileMenu = document.getElementById("mobile-menu");
+  const navLinks = document.getElementById("nav-links-desk");
+
+  mobileMenu.addEventListener("click", () => {
+    navLinks.classList.toggle("nav-active");
+    mobileMenu.classList.toggle("toggle");
+  });
+});
+
+let mobileNavOpen = true;
+
+function toggleMobileNav() {
+  const navLinks = document.getElementById("nav-links");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  if (mobileNavOpen) {
+    navLinks.classList.remove("nav-active");
+    mobileMenu.classList.remove("toggle");
+  } else {
+    navLinks.classList.add("nav-active");
+    mobileMenu.classList.add("toggle");
+  }
+
+  mobileNavOpen = !mobileNavOpen;
+}
